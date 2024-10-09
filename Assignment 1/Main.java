0322cf97e4db34cd4f60b3e900eb8c527159628c
Guglielmo Mazzesi <100280802+GuglielmoMazzesiDaniele@users.noreply.github.com>
2024-10-09 09:35:57 +0200

    Replaced a deprecated Integer initialization with a valid one.

--- Assignment 1/Main.java
+++ Assignment 1/Main.java
@@ -1,17 +1,21 @@
-import java.lang.reflect.Array;
 import java.util.Random;
 
+/**
+ * Main Class
+ */
 public class Main {
 
-
-    public static void main(String args[]){
+    /**
+     * Main function
+     * @param args Arguments provided by command lines
+     */
+    public static void main(String[] args){
 
         Integer[] array = new Integer[10];
         Random rand = new Random();
 
-
         for (int i = 0; i < array.length; i++ ){
-            array[i] = new Integer(rand.nextInt(30));
+            array[i] = rand.nextInt(1000);
             System.out.println(array[i].toString());
         }
     }
