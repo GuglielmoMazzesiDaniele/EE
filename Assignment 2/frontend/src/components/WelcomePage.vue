def3c91067e3972b6be787895ca68a86f4915dc9
IronJam <jkuma@protonmail.com>
2024-12-02 23:13:40 +0100

    First frontend draft
    Co-Authored-By: dariavikhr <125987809+dariavikhr@users.noreply.github.com>

---
+++ Assignment 2/frontend/src/components/WelcomePage.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <!-- Root div element is required -->
+      <div id="explanations">
+        lorem ipsum   lorem ipsum   lorem ipsum     lorem ipsum
+        lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsum
+        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+        lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+        lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+      </div>
+      TODO: Write the explanation of the experiment + instructions
+      <div>
+        <el-button @click="$emit('next')" type="primary">I understand and wish to proceed</el-button>
+      </div>
+
+</template>
+
+<script>
+export default {
+  name: 'WelcomePage',
+};
+</script>
+
+<style scoped>
+#explanations {
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
